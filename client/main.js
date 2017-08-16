1e8c11a56a8c2d56c54d592392af519b59471a81
Ramses <erik.rmh@gmail.com>
2017-08-15 20:08:12 -0500

    Ajustes Meteor

--- client/main.js
+++ client/main.js
@@ -4,6 +4,35 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+ 
+Meteor.startup(() => {
+	//ShareIt.configure({
+    //    sites: {
+    //        'facebook': {
+    //            'appId': 2077837752462304
+    //        }
+    //    }
+    //});
+     ShareIt.configure({
+    sites: {                // nested object for extra configurations
+        'facebook': {
+            'appId': 2077837752462304	// use sharer.php when it's null, otherwise use share dialog
+        },
+        'twitter': {},
+        'googleplus': {},
+        'pinterest': {}
+    },
+    classes: "large btn", // string (default: 'large btn')
+                          // The classes that will be placed on the sharing buttons, bootstrap by default.
+    iconOnly: false,      // boolean (default: false)
+                          // Don't put text on the sharing buttons
+    applyColors: true,     // boolean (default: true)
+                          // apply classes to inherit each social networks background color
+    faSize: '',            // font awesome size
+    faClass: ''		  // font awesome classes like square
+  	});
+});
+
 
 Template.main.helpers({
 });
@@ -25,6 +54,25 @@ Template.posts.events({
       });
       alert('Orden creada correctamente');
     },
+    'click a.addField': function(event, template) 
+  	{
+  		event.preventDefault();
+  		var nuevocampo = 'campodeEjemplo';
+      	Posts.update(this._id, {
+           $set: { nuevocampo: 'Texto ejemplo'}
+        });
+        Meteor.call('addField', nuevocampo, this._id, function(error, result) {
+		});
+      	alert('Campo cambiado correctamente');
+    },
+    'click a.deleteField': function(event, template) 
+  	{
+  		event.preventDefault();
+  		var nuevocampo = 'campodeEjemplo';
+        Meteor.call('deleteField', nuevocampo, this._id, function(error, result) {
+		});
+      	alert('Campo eliminado correctamente');
+    }
 });
 
 Template.posts.helpers({
